7dba374690ce4ca096239ff000e7b5a15779d81a
JustinMig <Justin@mayerig.com>
2026-09-16 16:33:46 -0500

    Refresh doctor specialty dropdown module

--- workspace-extensions.js
+++ workspace-extensions.js
@@ -6,7 +6,7 @@ import './banking-information.js?v=client-open-1';
 import './life-multiple-policies.js?v=client-open-1';
 import './life-other-carrier.js';
 import './client-pharmacy.js';
-import './client-health-tabs.js?v=medication-add-bottom-1';
+import './client-health-tabs.js?v=doctor-specialty-1';
 import './medication-autocomplete.js?v=rxterms-1';
 import './hospital-indemnity-documents.js';
 import './client-personal-underwriting.js';
