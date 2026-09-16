1b7f7b3335d03ae8fe40e7035739b95974aeb8aa
JustinMig <Justin@mayerig.com>
2026-09-16 06:56:08 -0500

    Force refresh permanent New Client toolbar module

--- workspace-start.js
+++ workspace-start.js
@@ -47,7 +47,7 @@ export async function startWorkspace() {
   await import('./input-formatting.js?v=2');
   await import('./campaigns-smooth-load.js?v=1');
   createWorkspace(root, mhRepository);
-  await import('./top-add-client.js?v=2');
+  await import('./top-add-client.js?v=3');
   installClientAgeFilter(root);
   installClientDelete(root, mhRepository);
   installSimpleDashboardNote(root);
